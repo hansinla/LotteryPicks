7324965722f0f9c6e660f01dd9fb0a3470d78409
hansinla <hansinla@mac.com>
2016-01-09 16:10:37 -0800

    Added some comments

--- LotteryPicks/main.swift
+++ LotteryPicks/main.swift
@@ -1,17 +1,14 @@
-//
 //  main.swift
 //  LotteryPicks
 //
 //  Created by Hans van Riet on 11/10/14.
 //  Copyright (c) 2014 Hans van Riet. All rights reserved.
-//
 
 import Foundation
 
-/* 
-    PowerBall Rules:
-    5 numbers from 1 to 75 (inclusive)
-    1 Mega number from 1 to 15 (inclusive)
+/*  PowerBall Rules:
+    5 numbers from 1 to 69 (inclusive)
+    1 PowerBall number from 1 to 26 (inclusive)
 */
 
 let bottomNumber = 1
@@ -26,20 +23,17 @@ let numMega = 1
 func drawNumbers(lowerLimit: Int, upperLimit: UInt32, amount: Int) -> Array<Int> {
     
     var currentPick: Int
+    var lotteryPicks = Array<Int>() // init an empty array
     
-    // init an empty array
-    var lotteryPicks = Array<Int>()
-    
-    // Loop for until array is full
     repeat {
         // Generate a random number and put it in the array if it's unique
         currentPick = lowerLimit + Int(arc4random_uniform(upperLimit))
         if !(lotteryPicks.contains(currentPick)) {
                 lotteryPicks.append(currentPick)
             }
-    } while (lotteryPicks.count < amount)
+    } while (lotteryPicks.count < amount) // Loop for until array is full
     
-    lotteryPicks.sortInPlace { $0 < $1 }
+    lotteryPicks.sortInPlace { $0 < $1 } // sort the array for output
     
     return lotteryPicks
 }
@@ -54,6 +48,5 @@ for _ in 1...10 {
     print("Powerball number: ", terminator: "")
     print(drawNumbers(bottomMega, upperLimit: topMega, amount: numMega))
     
-
 
 }
